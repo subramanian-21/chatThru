6b11d1e8d45badcad927172e17b716788eebef5e
thugperfect <meenamani9444@gmail.com>
2023-07-28 08:37:12 +0000

    mongoDb connected..

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ require('dotenv').config()
 const app = express()
 const port =process.env.PORT
 
+const mongo_url = process.env.MONGO_URL
+
+mongoose.connect(mongo_url)
+    .catch(err => console.log(err)
+  
+    )
+
 app.listen(port ,()=>{
     console.log('server Started at ',port);
 })

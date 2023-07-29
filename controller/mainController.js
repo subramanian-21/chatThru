4ddf65ea2fbc8c22c2c4f5637cce93c5291353f3
thugperfect <meenamani9444@gmail.com>
2023-07-29 10:12:29 +0000

    created access tokens for creation of new user

--- controller/mainController.js
+++ controller/mainController.js
@@ -1,6 +1,10 @@
 const mainModel = require('../model/mainModel')
 const MainSchema = require('../model/mainModel')
 
+const newAccessToken = (user) =>{
+    return jwt.sign(user,process.env.ACCESS_TOKEN,{expire : '1d'})
+}
+
 const mainController = {
     register: async(req,res) =>{
         try{
@@ -18,8 +22,9 @@ const mainController = {
             if(password.length < 8){
                 return res.status(400).json({message:"Password must be more than 8 characters..."})
             }
-            const newUser = new mainModel({
-                fullname,username,email,password
+            const newUser = new m
+            ainModel({
+                firstname,lastname,username,email,password
             })
         }
         catch{

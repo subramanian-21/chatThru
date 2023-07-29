4bb4194bdbcc7c5bc58944f1a21017c17287aca0
thugperfect <meenamani9444@gmail.com>
2023-07-29 09:27:27 +0000

    main schema for user

---
+++ model/mainModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const mainSchema = new mongoose.Schema({
+    firstname:{
+        type:String,
+        required:true,
+        maxlength:25,
+        trim:true
+    },
+    lastname:{
+        type:String,
+        required:true,
+        maxlength:25,
+        trim:true
+    },
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        uniquie:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+
+})

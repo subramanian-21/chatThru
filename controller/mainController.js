9ce446551b8cc056bbd802bb7bf192e58d074dc8
thugperfect <meenamani9444@gmail.com>
2023-07-29 09:50:56 +0000

    username and email unique verification...

--- controller/mainController.js
+++ controller/mainController.js
@@ -4,9 +4,15 @@ const mainController = {
     register: async(req,res) =>{
         try{
             const {firstname,lastname,username,email,password} = req.body
-            const user_name = await MainSchema.findOne({username})
-            if(user_name) return res.status(400).json({message:"Username already taken..."})
+            const user_name_check = await MainSchema.findOne({username})
+            if(user_name_check){
+                return res.status(400).json({message:"Username already taken..."})
+            } 
             
+            const user_email_check = await Users.findOne({email})
+            if(user_email_check) {
+                return res.status(400).json({message:"Email already taken..."})
+            }
 
         }
         catch{

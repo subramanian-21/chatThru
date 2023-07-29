57a4694e207966195fc2f70479e0cb5e48bdc5bc
thugperfect <meenamani9444@gmail.com>
2023-07-29 09:56:08 +0000

    backend registration ...

--- controller/mainController.js
+++ controller/mainController.js
@@ -1,3 +1,4 @@
+const mainModel = require('../model/mainModel')
 const MainSchema = require('../model/mainModel')
 
 const mainController = {
@@ -14,6 +15,12 @@ const mainController = {
                 return res.status(400).json({message:"Email already taken..."})
             }
 
+            if(password.length < 8){
+                return res.status(400).json({message:"Password must be more than 8 characters..."})
+            }
+            const newUser = new mainModel({
+                fullname,username,email,password
+            })
         }
         catch{
         

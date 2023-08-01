fce7c667efe3f941aa8b3690f29cce206f21a4e4
thugperfect <meenamani9444@gmail.com>
2023-08-01 10:52:06 +0000

    Login page...

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,7 +1,44 @@
 import "./pages.scss"
 function Login() {
-    return(<div className="login-body">
-<div className="h1">Login Page</div>
-    </div>)
+    return( <div className="loginbody">
+         
+
+    <div className='loginbox'>
+
+
+    <h3 className="logo">ChatThru</h3>
+    <form>
+   
+        
+        <input type="email" className="inp" id="exampleInputEmail1" name="email"
+        aria-describedby="emailHelp" placeholder = "Enter your Email..." />
+        
+   
+
+  
+       
+
+        <div className="pass" id='passwd'>
+            
+            <input 
+            className="inp"  placeholder='Enter Passwd...' 
+            name="password" />
+
+            <small className="sh" >
+
+            </small>
+        </div>
+       
+    
+    <button type="submit" className="inp" id='sub'>
+        Login
+    </button>
+
+   </form>
+   <p className="my-2">
+        Don't have an account? <a id='dont'></a>
+    </p></div>
+
+</div>)
 }
 export default Login
